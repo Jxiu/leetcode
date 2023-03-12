d5a4db943d01a9146798ac1d4a02495b54111524
jxiu <956832164@qq.com>
2023-03-12 08:28:15 +0800

    leetcode 503. 下一个更大元素 II(medium) 单调栈

---
+++ algorithm/java/src/main/java/com/github/jxiu/doing/NextGreaterElementII.java
@@ -0,0 +1,37 @@
+package com.github.jxiu.doing;
+
+import java.util.Stack;
+
+/**
+ * 503. 下一个更大元素 II
+ */
+public class NextGreaterElementII {
+
+    public int[] nextGreaterElements(int[] nums) {
+        // 先找到最先出现的最大值下标
+        int maxIdx = 0;
+        for (int i = 1; i < nums.length; i++) {
+            if (nums[i] > nums[maxIdx]){
+                maxIdx = i;
+            }
+        }
+        Stack<Integer> stk = new Stack<>();
+        int[] ans = new int[nums.length];
+        for (int i = maxIdx+nums.length; i > maxIdx; i--) {
+            int ai = i%nums.length; // 数组中的坐标
+            // 维护一个单调的递增的栈
+            while(!stk.isEmpty() && nums[ai] >= stk.peek()){
+                // 栈不满足单调递增，弹出所有小于等于当前元素的栈
+                stk.pop();
+            }
+            // 计算出
+//            ans[ai] = stk.empty() ? -1 : Math.abs(stk.peek() - i);
+            ans[ai] =  stk.empty() ? -1 : stk.peek();
+            //推入当前元素
+            stk.push(nums[ai]);
+            System.out.println(stk);
+            // 重新计算最大值之后的元素
+        }
+        return  ans;
+    }
+}
