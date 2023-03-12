729b02cbe88e24bef36c0fd942ca95fa44663f18
jxiu <956832164@qq.com>
2023-03-13 06:51:49 +0800

    leetcode 697. Degree of an Array(easy)  hash表

---
+++ algorithm/java/src/test/java/com/github/jxiu/doing/DegreeOfAnArrayTest.java
@@ -0,0 +1,28 @@
+package com.github.jxiu.doing;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class DegreeOfAnArrayTest {
+
+    @ParameterizedTest
+    @MethodSource
+    public void successTest(int[] in, int expected){
+        DegreeOfAnArray issue = new DegreeOfAnArray();
+        Assertions.assertEquals(expected, issue.findShortestSubArray(in));
+    }
+
+    static Stream<Arguments> successTest(){
+        return Stream.of(
+                Arguments.of(new int[]{1,2,2,3,1},2),
+                Arguments.of(new int[]{1,2,2,3,1,4,2},6)
+                );
+    }
+
+}
