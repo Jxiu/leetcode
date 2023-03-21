37d36fda9b45b86dd1ef4e8a0a5251de552ad5e0
jxiu <956832164@qq.com>
2023-03-21 08:48:07 +0800

    leetcode 64.Minimum Path Sum(medium) 最小路径和 动态规划

---
+++ algorithm/java/src/main/java/com/github/jxiu/doing/MinimumPathSum.java
@@ -0,0 +1,30 @@
+package com.github.jxiu.doing;
+
+/**
+ * 64. Minimum Path Sum(medium) 最小路径和
+ */
+public class MinimumPathSum {
+
+    public int minPathSum(int[][] grid) {
+        // 数组中数字都是非负数，初始化dp数组
+        int N = grid.length; int IN = N == 0 ? 0 : grid[0].length;
+        // 多初始化一层右上角默认都是0
+        int[][] dp = new int[N][IN];
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < IN; j++) {
+                // 最上边
+                if (i == 0 && j == 0){
+                    dp[i][j] = grid[i][j];
+                } else if (i == 0 && j > 0){
+                    dp[i][j] = dp[i][j-1] + grid[i][j];
+                } else if (j == 0 && i > 0){
+                    dp[i][j] = dp[i-1][j] + grid[i][j];
+                }else {
+                    dp[i][j] = Integer.min(dp[i][j-1], dp[i-1][j]) + grid[i][j];
+                }
+            }
+        }
+
+        return dp[N-1][IN-1];
+    }
+}
---
+++ algorithm/java/src/test/java/com/github/jxiu/doing/MinimumPathSumTest.java
@@ -0,0 +1,31 @@
+package com.github.jxiu.doing;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MinimumPathSumTest {
+
+    @ParameterizedTest
+    @MethodSource
+    public void successTest(int[][] in, int expected){
+        MinimumPathSum issue = new MinimumPathSum();
+
+        int out = issue.minPathSum(in);
+
+        Assertions.assertEquals(expected, out);
+    }
+
+    static Stream<Arguments> successTest(){
+        return Stream.of(
+                Arguments.of(new int[][]{{1,3,1},{1,5,1},{4,2,1}},7),
+                Arguments.of(new int[][]{{1,2,3},{4,5,6}},12)
+        );
+    }
+
+}
