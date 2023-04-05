6067df5beca2f74bd7b5de6965f220f7dcba2c49
jxiu <956832164@qq.com>
2023-04-06 06:42:13 +0800

    leetcode 206. 反转链表 (Easy) 指针问题

---
+++ algorithm/java/src/main/java/com/github/jxiu/doing/ReverseLinkedList.java
@@ -0,0 +1,91 @@
+package com.github.jxiu.doing;
+
+import java.util.Objects;
+
+/**
+ * 206. 反转链表 (Easy)
+ */
+public class ReverseLinkedList {
+
+    public static class ListNode {
+        int val;
+        ListNode next;
+        ListNode() {}
+        ListNode(int val) { this.val = val; }
+        ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+
+        public static ListNode valueOf(int[] a){
+            if (a == null){
+                return null;
+            }
+            ListNode dummy = new ListNode();
+            ListNode pre = null;
+            for (int i : a ){
+                ListNode curr = new ListNode(i);
+                if (dummy.next == null){
+                    dummy.next = curr;
+                }else {
+                    pre.next = curr;
+                }
+                    pre = curr;
+            }
+            return dummy.next;
+        }
+
+        @Override
+        public String toString() {
+            StringBuilder sb = new StringBuilder();
+            ListNode pre = new ListNode(0,this);
+            ListNode curr = null;
+            do {
+                curr = pre.next;
+                sb.append(curr.val).append(",");
+                pre = curr;
+            }while (curr.next != null);
+            if (sb.length() > 0 ){
+                sb.delete(sb.length()-1,sb.length());
+            }
+            return sb.toString();
+        }
+
+        @Override
+        public boolean equals(Object obj) {
+            if (obj == null){
+                return false;
+            }
+            if (! (obj instanceof ListNode)){
+                return false;
+            }
+            ListNode o = (ListNode) obj;
+            if (val != o.val){
+                return false;
+            }
+            if (Objects.equals(next,o.next)){
+                return true;
+            }
+            return false;
+        }
+    }
+
+    /**
+     * TODO 使用指针实现
+     * @param head
+     * @return
+     */
+    public ListNode reverseList(ListNode head) {
+        return recursive(head);
+    }
+
+    private ListNode recursive(ListNode node) {
+        if (node.next == null){
+            // 最后一个元素
+            return node;
+        }
+        ListNode next = node.next;
+        node.next = null;
+        ListNode head = recursive(next);
+        next.next = node;
+        return head;
+    }
+
+}
---
+++ algorithm/java/src/test/java/com/github/jxiu/doing/ReverseLinkedListTest.java
@@ -0,0 +1,37 @@
+package com.github.jxiu.doing;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.Arrays;
+import java.util.stream.Stream;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ReverseLinkedListTest {
+
+    @ParameterizedTest
+    @MethodSource
+    public void test(int[] in){
+        int[] copy = Arrays.copyOf(in,in.length);
+        for (int i = 0; i < copy.length/2; i++) {
+            int temp = copy[i];
+            copy[i] = copy[copy.length-i-1];
+            copy[copy.length-i-1] = temp;
+        }
+        System.out.println(ReverseLinkedList.ListNode.valueOf(copy));
+        Assertions.assertEquals(ReverseLinkedList.ListNode.valueOf(copy),new ReverseLinkedList().reverseList(ReverseLinkedList.ListNode.valueOf(in)));
+    }
+
+    static Stream<Arguments> test(){
+        return Stream.of(
+                Arguments.of(new int[]{1,2,3,4,5}),
+                Arguments.of(new int[]{1})
+
+        );
+    }
+
+}
